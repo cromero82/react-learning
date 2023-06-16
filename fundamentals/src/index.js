5451a1d96984e4a92162494a5486e8d91ce2b90b
carlos romero <soportecarlosromero@gmail.com>
2023-06-15 22:09:37 -0500

    vite based

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -4,14 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const element = <h1 className='header' This is JSX></h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//const root = ReactDOM.createRoot(element,document.getElementById('root'));
+ReactDOM.render(element, document.getElementById('root') );
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
