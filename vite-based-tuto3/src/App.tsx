7d3cd5109d0585df36c59d2c2f1526d180fb74ea
carlos romero <soportecarlosromero@gmail.com>
2023-06-19 15:50:55 -0500

    addin custom css Avatar

--- vite-based-tuto3/src/App.tsx
+++ vite-based-tuto3/src/App.tsx
@@ -1,20 +1,28 @@
-import { Fragment } from 'react'
 import './App.css'
+import './Custom.css';
+import Avatar from './components/Avatar';
 
 function App() {
 
+  // const cities = [
+  //   'New york',
+  //   'San francisco',
+  //   'Tokio'
+  // ]
+
+
+  // return (
+  //   <>
+  //     <h1>List</h1>
+  //     <ul className="list-group">
+  //       {cities.map((city) => <li key={city} className="list-group-item"> {city} </li>)}
+  //     </ul>
+  //   </ >
+  // );
+
   return (
-    <>
-      <h1>List</h1>
-      <ul className="list-group">
-        <li className="list-group-item disabled">Cras justo odio</li>
-        <li className="list-group-item">Dapibus ac facilisis in</li>
-        <li className="list-group-item">Morbi leo risus</li>
-        <li className="list-group-item">Porta ac consectetur ac</li>
-        <li className="list-group-item">Vestibulum at eros</li>
-      </ul>
-    </ >
-  );
+    <div> <Avatar /></div>
+  )
 }
 
 export default App
